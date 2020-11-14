3b53a44d0f7319d4df8d15f4a7e345b13f1127b7
Zick108 <zickkotun@gmail.com>
2020-11-15 02:31:49 +0630

    Player layout and others are updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+window.onscroll = function() {myFunction1()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction1() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 //Filter for search box of index page
 function myFunction() {
   var input, filter, table, tr, td, i;
